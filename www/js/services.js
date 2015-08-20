32114b6da8a382a17aede297c6a8b8d2ff5cb5f6
AnatFradin <anat.fradin@gmail.com>
2015-08-20 14:59:22 +0300

    Adding Mobile Sample application

---
+++ www/js/services.js
@@ -0,0 +1,49 @@
+angular.module('rtcommMobile.services', ['angular-rtcomm'])
+.factory('Video', function($rootScope, $log, RtcommService) {
+})
+
+.factory('Config', function($rootScope, $http, $log, RtcommService){
+  var configURL = "rtcommConfig.json";
+  var defaultConfig = {};
+  var localConfig = localStorage['rtcommConfig'] ? 
+                    JSON.parse(localStorage['rtcommConfig']) :  
+                    null;
+
+  $log.info('Using LocalStorage config: ', localConfig);
+  var config = localConfig || { enableVideo: true,
+    server: 'messagesight.ibm.com',
+    port: 1883,
+	  rtcommTopicPath : "/rtcomm/",
+    queue: 'Doctors',
+  };
+
+  $rootScope.$evalAsync(function(){
+    if(localConfig) {
+      angular.extend(config, localConfig);
+    } else {
+      $log.info('Loading config in Config Service...');
+      $http.get(configURL)
+        .success( function(cfg){
+          $log.info('Received cfg: ',cfg);
+          defaultConfig = cfg; 
+          angular.extend(config, defaultConfig);
+          $log.info('Config is now: ',config);
+          localStorage['rtcommConfig'] = JSON.stringify(config);
+        })
+        .error(function(data,status,headers,config){
+          $log.debug("Config Service unable to access config "+status);
+        });
+    }
+  });
+
+  return {
+    config: config,
+    set: function(){
+      localStorage['rtcommConfig'] = JSON.stringify(config);
+      RtcommService.setConfig(config);
+    },
+    getKey: function(key){
+      return config[key];
+    }
+  };
+});
